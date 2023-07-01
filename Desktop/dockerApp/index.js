c7804a852bfff71167e7197f5884105670b9d44e
RomarioNijim <romario@almond-qms.com>
2023-07-01 15:08:46 +0300

    changes

--- Desktop/dockerApp/index.js
+++ Desktop/dockerApp/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hi There!!!!</h1>")
+    res.send("<h1>Hello Docker</h1>")
 })
 const port = process.env.PORT || 3000
 
