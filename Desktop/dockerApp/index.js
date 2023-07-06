2183a44d6a40303eb33098c72fe1c3c65ed1aaf6
RomarioNijim <romario@almond-qms.com>
2023-07-06 22:14:08 +0300

    added few changes

--- Desktop/dockerApp/index.js
+++ Desktop/dockerApp/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 const app = express();
 
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+mongoose.connect(mongoUrl).then(() => console.log('successfully conencted to DB')).catch((e) => console.log(`error => ${e}`));
+
+
 app.get('/', (req, res) => {
-    res.send("<h1>Hello from Docker!!!!</h1>")
+    res.send("<h1>Hello from Docker!!!</h1>")
 })
 const port = process.env.PORT || 3000
 
