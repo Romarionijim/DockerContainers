9b524c2050dd3c4e1ebbc2433c447653a22b3431
RomarioNijim <romario@almond-qms.com>
2023-07-07 15:11:58 +0300

    add express CRUD

--- Desktop/dockerApp/index.js
+++ Desktop/dockerApp/index.js
@@ -1,23 +1,33 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
+const postRouter = require('./routes/postRoutes');
+const userRouter = require('./routes/userRoutes');
 const app = express();
 
-const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
 const connectWithRetry = () => {
-    mongoose.connect(mongoUrl).then(() => console.log('successfully conencted to DB'))
+    mongoose.connect(mongoUrl, {
+        serverSelectionTimeoutMS: 60000,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => console.log('successfully conencted to DB'))
         .catch((e) => {
             console.log(`error => ${e}`)
             setTimeout(connectWithRetry, 5000);
         });
 }
 
-
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("<h1>Hello from Docker!!!</h1>")
 })
+
+//localhost:3000/api/v1/posts/
+app.use('/posts', postRouter);
+app.use('/users', userRouter);
 const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`listening on port ${port}`));
